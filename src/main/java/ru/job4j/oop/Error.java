4681b2c85481d37e778b8f4d11fccb10419f56f2
SvFoxy <sveta_4_you_@mail.ru>
2023-10-11 15:49:54 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активный: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error err = new Error();
+        err.printInfo();
+
+        Error errWithParam = new Error(true, 1, "error");
+        errWithParam.printInfo();
+    }
+}
