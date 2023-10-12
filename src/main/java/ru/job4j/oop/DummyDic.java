1f25710e31bc07ad22a98df083e5961d4f2f8c19
SvFoxy <sveta_4_you_@mail.ru>
2023-10-12 13:07:05 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387 #451852]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,8 +2,6 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        String text = "Неизвестное слово. " + eng;
-        return text;
+        return "Неизвестное слово. " + eng;
     }
-
 }
