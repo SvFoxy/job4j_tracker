748fd84b72ae8880296fe252a6c4a7a02ec1bb31
SvFoxy <sveta_4_you_@mail.ru>
2023-10-19 13:27:04 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 @SuppressWarnings("checkstyle:WhitespaceAround")
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         String ln = System.lineSeparator();
         return "{" + ln
