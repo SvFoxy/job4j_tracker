a62893459408e709616eecb6914a5a04a218cc7e
SvFoxy <sveta_4_you_@mail.ru>
2023-10-25 12:20:27 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -22,4 +24,26 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        for (int index = 0; index < size; index++) {
+            rsl[index] = items[index];
+        }
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int len = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName().equals(key)) {
+                rsl[len] = items[index];
+                len++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, len);
+        return rsl;
+    }
+
 }
