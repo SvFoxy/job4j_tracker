33ee855dd43372307bbeff16d1565f4aec3f4cce
SvFoxy <sveta_4_you_@mail.ru>
2023-11-02 17:15:36 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,6 @@
+package ru.job4j.oop;
+
+public class College {
+    Student freshman = new Freshman();
+    Object obj = freshman;
+}
--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -17,4 +17,5 @@ public class Student {
         petya.sing();
         petya.sing();
     }
+
 }
