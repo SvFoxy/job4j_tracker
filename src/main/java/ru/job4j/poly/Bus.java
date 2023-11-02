0c6cdbeb70075bcea02cb7ae9d69f255d9341cc2
SvFoxy <sveta_4_you_@mail.ru>
2023-11-02 15:52:05 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public int fill(int count) {
+        return 0;
+    }
+}
