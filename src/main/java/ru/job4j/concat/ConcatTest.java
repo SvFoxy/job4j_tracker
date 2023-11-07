264ee511049a19a7226d533966300acc2ab5e6a2
SvFoxy <sveta_4_you_@mail.ru>
2023-11-07 15:59:43 +0300

    from lessons

--- src/main/java/ru/job4j/concat/ConcatTest.java
+++ src/main/java/ru/job4j/concat/ConcatTest.java
@@ -3,15 +3,15 @@ package ru.job4j.concat;
 public class ConcatTest {
   /*  public static void main(String[] args) {
 
-        long startTime = System.currentTimeMillis(); // фиксируем время старта программы в миллисекундах
+        long startTime = System.currentTimeMillis();  фиксируем время старта программы в миллисекундах
         String str = "Job4j";
         for (int index = 0; index < 9999; index++) {
-            //"String concatenation '+' in loop", "String concatenation '+' in loop",
-           // Такое слияние имеет очень низкую производительность./
+            "String concatenation '+' in loop", "String concatenation '+' in loop",
+            Такое слияние имеет очень низкую производительность.
             str = str + index;
         }
-       // время работы программы рассчитываем в виде вычитания времени старта программы
-       //  из времени её фактического окончания в строке *
+        время работы программы рассчитываем в виде вычитания времени старта программы
+         из времени её фактического окончания в строке *
         System.out.println("Миллисекунд: " + (System.currentTimeMillis() - startTime));
     }*/
 
