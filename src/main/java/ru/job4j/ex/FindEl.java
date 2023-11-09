495510cb5774746bbd24b5e7c2f27998297a78da
SvFoxy <sveta_4_you_@mail.ru>
2023-11-09 18:49:51 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws UserInputException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new UserInputException("index not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] str = {"asd", "fgh", "jkl"};
+            indexOf(str, "dsa");
+        } catch (UserInputException e) {
+            e.printStackTrace();
+        }
+    }
+}
