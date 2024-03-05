fa180852e6ca6fded2feb564798a5b8cce9f69f9
SvFoxy <sveta_4_you_@mail.ru>
2024-03-05 10:48:09 +0300

    0. Map, HashMap [#212652 #484547]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("sveta@mail.ru", "Frolova Svetlana A");
+        map.put("stas@mail.ru", "Korobeinikov Stas");
+        map.put("stas@mail.ru", "Stas");
+        map.put("petr@mail.ru", "Arsentiev Petr");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
