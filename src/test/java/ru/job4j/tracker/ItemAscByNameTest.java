122980be8c4e79664a79b20367648e2e7033f400
SvFoxy <sveta_4_you_@mail.ru>
2024-03-07 10:07:41 +0300

    0. Сортировка [#10096 #484823]

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -2,23 +2,25 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ItemAscByNameTest {
     @Test
     void compareItemAscByName() {
-        List<Item> items = new ArrayList<>();
-        items.add(0, new Item("A"));
-        items.add(1, new Item("C"));
-        items.add(2, new Item("B"));
-        List<Item> expected = new ArrayList<>();
-        expected.add(items.get(0));
-        expected.add(items.get(2));
-        expected.add(items.get(1));
+        List<Item> items = Arrays.asList(
+                new Item("A"),
+                new Item("C"),
+                new Item("B")
+        );
         items.sort(new ItemAscByName());
+        List<Item> expected = Arrays.asList(
+                new Item("A"),
+                new Item("B"),
+                new Item("C")
+        );
         assertThat(items).isEqualTo(expected);
     }
 }
--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -2,8 +2,8 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +11,20 @@ class ItemDescByNameTest {
 
     @Test
     void compareItemDescByName() {
+
+        List<Item> items = Arrays.asList(
+                new Item("A"),
+                new Item("C"),
+                new Item("B")
+        );
+        items.sort(new ItemDescByName());
+
+        List<Item> expected = Arrays.asList(
+                new Item("C"),
+                new Item("B"),
+                new Item("A")
+        );
+        /*
         List<Item> items = new ArrayList<>();
         items.add(0, new Item("A"));
         items.add(1, new Item("C"));
@@ -19,7 +33,7 @@ class ItemDescByNameTest {
         expected.add(items.get(1));
         expected.add(items.get(2));
         expected.add(items.get(0));
-        items.sort(new ItemDescByName());
+        items.sort(new ItemDescByName());*/
         assertThat(items).isEqualTo(expected);
     }
 }
