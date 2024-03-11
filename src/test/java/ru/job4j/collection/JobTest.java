f0cdbf6a65ada9aca754f9318750f9309bb00df5
SvFoxy <sveta_4_you_@mail.ru>
2024-03-11 16:38:56 +0300

    2. Комбинированный компаратор [#10036 #484882]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -5,22 +5,13 @@ import java.util.Comparator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JobTest {
-    @Test
-    public void whenCompatorByNameAndPrority() {
-        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
-        int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
-        );
-        assertThat(rsl).isLessThan(0);
-    }
 
     @Test
     public void whenCompatorAscByNameAndPrority() {
         Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Fix bug", 1),
-                new Job("Impl task", 0)
+                new Job("Fix bug", 0),
+                new Job("Fix bug", 1)
         );
         assertThat(rsl).isLessThan(0);
     }
@@ -64,4 +55,15 @@ public class JobTest {
         );
         assertThat(rsl).isLessThan(0);
     }
+
+    @Test
+    public void whenCompatorDescByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 0)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
 }
